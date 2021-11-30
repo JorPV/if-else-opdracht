51c622d61f54b4e36d292ced20efabb0630eab6d
JordiPV <88150345+JorPV@users.noreply.github.com>
2021-11-30 17:08:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const Age = 20; 
+const isFemale = true; 
+const driverStatus = "bob"; 
+bob = "This driver is allowed to drive";
+
+
+// Part 1
+if (Age == 18){
+
+    document.write("This person is 18");
+    console.log ("This person is 18");
+
+} else if (Age < 18){
+
+    console.log ("This person is yonger than 18 and can't enter the pub");
+
+} else {
+
+    console.log("This person is allowed because is older than 18");
+}
+
+// Part 2
+if (isFemale){
+
+    console.log("This person is a woman");
+
+} else{
+
+    console.log("This person is not a woman");
+}
+
+// Part 3'
+if (driverStatus = bob){
+
+    console.log("bob"); 
+
+} else{
+
+    console.log("This driver isn't allowed to drive")
+}
